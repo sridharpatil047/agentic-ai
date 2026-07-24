ec5688062dc4654f95db48c911bfc500883aabaa
sridharpatil047 <sridharpatil047@gmail.com>
2026-07-25 01:10:33 +0530

    restructring the examples

--- tools.py
+++
@@ -1,9 +0,0 @@
-from langchain.tools import tool
-
-
-@tool
-def get_current_time() -> str:
-    """Return the current UTC time."""
-    from datetime import datetime, timezone
-
-    return datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S UTC")
